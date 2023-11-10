98a96bbef7580a917dd2cd36d158f60bbacfb267
cirqueira <118860604+CirqueiraDev@users.noreply.github.com>
2023-11-10 16:21:31 -0300

    Update cnc.py

--- src/cnc.py
+++ src/cnc.py
@@ -65,40 +65,26 @@ def text2Gen():
     return colored_word
 
 def color(data_input_output):
-    random_output = data_input_output
-    if random_output == "GREEN":
-        data = '\033[32m'
-    elif random_output == "LIGHTGREEN_EX":
-        data = '\033[92m'
-    elif random_output == "YELLOW":
-        data = '\033[33m'
-    elif random_output == "LIGHTYELLOW_EX":
-        data = '\033[93m'
-    elif random_output == "CYAN":
-        data = '\033[36m'
-    elif random_output == "LIGHTCYAN_EX":
-        data = '\033[96m'
-    elif random_output == "BLUE":
-        data = '\033[34m'
-    elif random_output == "LIGHTBLUE_EX":
-        data = '\033[94m'
-    elif random_output == "MAGENTA":
-        data = '\033[35m'
-    elif random_output == "LIGHTMAGENTA_EX":
-        data = '\033[95m'
-    elif random_output == "RED":
-        data = '\033[31m'
-    elif random_output == "LIGHTRED_EX":
-        data = '\033[91m'
-    elif random_output == "BLACK":
-        data = '\033[30m'
-    elif random_output == "LIGHTBLACK_EX":
-        data = '\033[90m'
-    elif random_output == "WHITE":
-        data = '\033[37m'
-    elif random_output == "LIGHTWHITE_EX":
-        data = '\033[97m'
-    return data
+    color_codes = {
+        "GREEN": '\033[32m',
+        "LIGHTGREEN_EX": '\033[92m',
+        "YELLOW": '\033[33m',
+        "LIGHTYELLOW_EX": '\033[93m',
+        "CYAN": '\033[36m',
+        "LIGHTCYAN_EX": '\033[96m',
+        "BLUE": '\033[34m',
+        "LIGHTBLUE_EX": '\033[94m',
+        "MAGENTA": '\033[35m',
+        "LIGHTMAGENTA_EX": '\033[95m',
+        "RED": '\033[31m',
+        "LIGHTRED_EX": '\033[91m',
+        "BLACK": '\033[30m',
+        "LIGHTBLACK_EX": '\033[90m',
+        "WHITE": '\033[37m',
+        "LIGHTWHITE_EX": '\033[97m',
+    }
+
+    return color_codes.get(data_input_output, "")
 
 lightwhite = color("LIGHTWHITE_EX")
 gray = color("LIGHTBLACK_EX")
